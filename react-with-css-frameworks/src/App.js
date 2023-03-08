0bd4b1c6edc50123ae5267d1d136655f4f2c6722
guilhermescr <guilhermechaves2005@hotmail.com>
2023-03-08 17:49:27 -0300

    added some projects and a homepage to link to other projects

---
+++ react-with-css-frameworks/src/App.js
@@ -0,0 +1,12 @@
+import StyledButton from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Button using styled components</h1>
+      <StyledButton>Click Me</StyledButton>
+    </div>
+  );
+}
+
+export default App;
